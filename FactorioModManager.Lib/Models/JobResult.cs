a0a144bf4bc56193312bc2ac690a4d1ab9f29515
Tristyn Stimpson <tristynstimpson@gmail.com>
2016-09-29 23:33:18 -0700

    Add JobResult

---
+++ FactorioModManager.Lib/Models/JobResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace FactorioModManager.Lib.Models
+{
+    public class JobResult
+    {
+        public virtual string[] ErrorMessages { get; private set; }
+
+        public virtual bool IsSuccess => !ErrorMessages.Any();
+
+        public static readonly JobResult Success = new JobResult();
+
+        private static readonly string[] EmptyErrorMessagesSingleton = new string[0];
+
+        public JobResult()
+        {
+            ErrorMessages = EmptyErrorMessagesSingleton;
+        }
+
+        public JobResult(params string[] errorMessages)
+        {
+            if (errorMessages == null)
+                throw new ArgumentNullException("errorMessages");
+
+            if(errorMessages.Any(string.IsNullOrWhiteSpace))
+                throw new ArgumentNullException("errorMessages", "The array contained an element that was null or whitespace.");
+            
+            ErrorMessages = errorMessages;
+        }
+    }
+}
--- FactorioModManager.Lib/Models/ModDependencyMetadata.cs
+++ FactorioModManager.Lib/Models/ModDependencyMetadata.cs
@@ -1,24 +1,53 @@
-﻿namespace FactorioModManager.Lib.Models
+﻿using System;
+
+namespace FactorioModManager.Lib.Models
 {
     public class ModDependencyMetadata
     {
-        // ctor
-        // validation
-
-        public string ModName { get; internal set; }
-
-        public string VersionName { get; internal set; }
-
-        public Version UpperBound { get; internal set; }
-
-        public VersionComparator UpperBoundComparator { get; internal set; }
-
-        public Version LowerBound { get; internal set; }
+        private readonly string _modName;
         
-        public VersionComparator LowerBoundComparator { get; internal set; }
+        public string ModName { get; internal set; }
+        
+        public Version RequiredVersionUpperBound { get; internal set; }
+
+        public VersionComparator RequiredVersionUpperBoundComparator { get; internal set; }
+
+        public Version RequiredVersionLowerBound { get; internal set; }
+        
+        public VersionComparator RequiredVersionLowerBoundComparator { get; internal set; }
+
+        public ModDependencyMetadata()
+        {
+            
+        }
+
+        public ModDependencyMetadata(
+            string modName, 
+            Version requiredVersionUpperBound = null, 
+            VersionComparator requiredVersionUpperBoundComparator = VersionComparator.None, 
+            Version requiredVersionLowerBound = null, VersionComparator 
+            requiredVersionLowerBoundComparator = VersionComparator.None)
+        {
+            ModName = modName;
+            RequiredVersionUpperBound = requiredVersionUpperBound;
+            RequiredVersionUpperBoundComparator = requiredVersionUpperBoundComparator;
+            RequiredVersionLowerBound = requiredVersionLowerBound;
+            RequiredVersionLowerBoundComparator = requiredVersionLowerBoundComparator;
+        }
+
+
+        public JobResult IsValid()
+        {
+            if(string.IsNullOrWhiteSpace(ModName))
+                return new JobResult("The mod name is required.");
+
+            return JobResult.Success;
+        }
+
 
         public enum VersionComparator : byte
         {
+            None = 0,
             EqualTo = 1,
             GreaterThan = 2,
             LessThan = 3,
