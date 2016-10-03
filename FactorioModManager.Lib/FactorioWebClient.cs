4cfa3a19cd0097d6800b1398c25e79227e77f322
Tristyn Stimpson <tristynstimpson@gmail.com>
2016-10-03 14:45:45 -0700

    Updated notes in web client

--- FactorioModManager.Lib/FactorioWebClient.cs
+++ FactorioModManager.Lib/FactorioWebClient.cs
@@ -4,18 +4,20 @@ namespace FactorioModManager.Lib
 {
     public class FactorioWebClient : IModPortalClient
     {
-
         /*
+Game downloading notes:
 factorio.com/get-download/{version}/{build}/{platform}
 
 build: alpha, demo, headless (only linux64 platform)
 
-platform: win64, win64-manual, win32 win32-manual, osx, linux64, linux32,
+platform: win64: .exe, win64-manual: .zip, win32, .exe win32-manual: .zip, osx: .dmg, linux64: .tar.gz, linux32: .tar.gz,
+
 
 
 factorio package feeds
 /download: stable release
 /download/experimental: experimental release
+
 query body > div.container > h3 to scrape version numbers
          */
 
